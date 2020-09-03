f1b7334c0af1a923395e0cfa1d51c92da3c50857
Deimos451 <minkevichdenis@gmail.com>
2020-09-03 09:32:40 +0300

    update

--- ngaek.py
+++ ngaek.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
                 try:
                     last_id = get_last_post_unix()
                     post = api.wall.get(owner_id= -40400418, domain= 'https://vk.com/public40400418', count= 1, v= 5.103)['items'][0]
-                    if (post['date'] > last_post_unix()) and ((post['text'] == '') or ('UPD' in post['text'])) and (len(post['attachments']) == 1):
+                    if (post['date'] > get_last_post_unix()) and ((post['text'] == '') or ('UPD' in post['text'])) and (len(post['attachments']) == 1):
                         img_url = post['attachments'][-1]['photo']['sizes'][-1]['url']
                         urllib.request.urlretrieve(img_url, './ngaek.jpg') 
                         image = open('./ngaek.jpg', 'rb')
