42fd738717a93a6c819a524804544c355eab9a9e
Deimos451 <minkevichdenis@gmail.com>
2020-12-11 23:07:09 +0300

    update post conditionals
    Set offset to 1, for get latest
    VK post. Update reposting to TG if with conditional "attachments" in the VK post dict

--- ngaek.py
+++ ngaek.py
@@ -5,6 +5,7 @@ import time, datetime
 import psycopg2
 import traceback
 import os
+import json
 
 DATABASE_URL = os.environ['DATABASE_URL']
 TELEBOT_TOKEN = os.environ['TELEBOT_TOKEN']
@@ -34,8 +35,13 @@ if __name__ == "__main__":
             while 1:
                 try:
                     last_id = get_last_post_unix()
-                    post = api.wall.get(owner_id= -40400418, domain= 'https://vk.com/public40400418', count= 1, v= 5.103)['items'][0]
-                    if (post['date'] > get_last_post_unix()) and ((post['text'] == '') or ('UPD' in post['text'])) and (len(post['attachments']) == 1):
+                    post = api.wall.get(owner_id= -40400418, domain= 'https://vk.com/public40400418', offset= 1, count= 1, v= 5.103)['items'][0]
+                    if (
+                            post['date'] > get_last_post_unix()                 and
+                            (post['text'] == '') or ('UPD' in post['text'])     and
+                            'attachments' in post                               and 
+                            len(post['attachments']) == 1
+                        ):
                         img_url = post['attachments'][-1]['photo']['sizes'][-1]['url']
                         urllib.request.urlretrieve(img_url, './ngaek.jpg') 
                         image = open('./ngaek.jpg', 'rb')
